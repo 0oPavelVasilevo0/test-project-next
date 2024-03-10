52a5a593da38c217c5960b6b12deef24821b6ec5
Pavel Vasilev <karterpahan@gmail.com>
2024-03-10 13:20:27 +0100

    creating listPost/listWorks

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -1,3 +1,35 @@
+const listPosts = [
+    {
+        id: 1,
+        name: "Making a design system from scratch",
+        date: "12 Feb 2020",
+        skills: "Design, Pattern",
+        p: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+    },
+    {
+        id: 2,
+        name: "Creating pixel perfect icons in Figma",
+        date: "12 Feb 2022",
+        skills: "Figma, Icon Design",
+        p: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+    },
+    {
+        id: 3,
+        name: "UI Interactions of the week",
+        date: "12 Feb 2021",
+        skills: "Express, Handlebars",
+        p: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+    },
+    {
+        id: 4,
+        name: "Creating pixel perfect",
+        date: "12 Feb 2019",
+        skills: "Design",
+        p: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+    },
+
+]
+
 export default function Posts() {
     return (
         <div className="posts-container">
@@ -6,16 +38,15 @@ export default function Posts() {
                 <a href="">View all</a>
             </div>
             <div className="posts">
-                <div className="post">
-                    <h2>Making a design system from scratch</h2>
-                    <p className="date">12 Feb 2020<span className="line"></span>Design, Pattern</p>
-                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-                </div>
-                <div className="post">
-                    <h2>Creating pixel perfect icons in Figma</h2>
-                    <p className="date">12 Feb 2020<span className="line"></span>Figma, Icon Design</p>
-                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-                </div>
+                {listPosts.map((post) => {
+                    return (
+                        <div key={post.id} className="post">
+                            <h2>{post.name}</h2>
+                            <p>{post.date}<span className='line'></span>{post.skills}</p>
+                            <p>{post.p}</p>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
